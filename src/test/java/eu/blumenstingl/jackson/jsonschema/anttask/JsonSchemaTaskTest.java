bd29617b6e3baafc624140ee0762b128d6249e51
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2016-02-20 14:10:56 +0100

    Remove unnecessary annotations from the test

--- src/test/java/eu/blumenstingl/jackson/jsonschema/anttask/JsonSchemaTaskTest.java
+++ src/test/java/eu/blumenstingl/jackson/jsonschema/anttask/JsonSchemaTaskTest.java
@@ -6,17 +6,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.BuildFileTest;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 
 public class JsonSchemaTaskTest extends BuildFileTest {
 
 	private Path tempDir;
 	
-	@Before
 	public void setUp() throws Exception {
 		super.setUp();
 
@@ -24,34 +19,28 @@ public class JsonSchemaTaskTest extends BuildFileTest {
 		configureProject("src/test/resources/ant-jsonschema-tests.xml");
 	}
 
-	@After
 	public void tearDown() throws Exception {
 		super.tearDown();
 
 		FileUtils.deleteDirectory(tempDir.toFile());
 	}
 
-	@Test(expected = BuildException.class)
 	public void testTask_noClassnameGiven() throws IOException {
 		expectBuildException("noClassnameGiven", "no classname property is given");
 	}
 
-	@Test(expected = BuildException.class)
 	public void testTask_noDestfileGiven() throws IOException {
 		expectBuildException("noDestfileGiven", "no destfile property is given");
 	}
 
-	@Test(expected = BuildException.class)
 	public void testTask_emptyClassnameGiven() throws IOException {
 		expectBuildException("emptyClassnameGiven", "a non-empty classname is required");
 	}
 
-	@Test(expected = BuildException.class)
 	public void testTask_destfileIsADirectory() {
 		expectBuildException("destfileIsADirectory", "destfile points to a directory");
 	}
 
-	@Test
 	public void testTask_mustAllowDestfileInExistingSubdirectory() {
 		executeTarget("mustAllowDestfileInExistingSubdirectory");
 
@@ -60,7 +49,6 @@ public class JsonSchemaTaskTest extends BuildFileTest {
 		assertTrue("The JSON schema must be written to the existing subdir", result.isFile());
 	}
 
-	@Test
 	public void testTask_createMissingParentDirectories() {
 		executeTarget("createMissingParentDirectories");
 
@@ -71,7 +59,6 @@ public class JsonSchemaTaskTest extends BuildFileTest {
 		assertTrue("The JSON schema must be written to the new directory", result.isFile());
 	}
 
-	@Test
 	public void testTask_createJsonSchema() {
 		executeTarget("createJsonSchema");
 
